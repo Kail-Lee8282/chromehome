d0486cd1812a6e0df27d210ec698a6adc89910dd
kmlee <goldtore@gmail.com>
2022-08-16 08:18:41 +0900

    modified image url

--- js/background.js
+++ js/background.js
@@ -5,8 +5,7 @@ const images = [
   "https://images.pexels.com/photos/2559941/pexels-photo-2559941.jpeg",
   "https://images.pexels.com/photos/11577587/pexels-photo-11577587.jpeg",
   "https://images.pexels.com/photos/4943085/pexels-photo-4943085.jpeg",
-  "https://images.pexels.com/photos/2885320/pexels-photo-2885320.jpeg",
-  "https://blogfiles.pstatic.net/MjAxODAzMTlfMjI3/MDAxNTIxNDQ4Nzg1Njc2.z4XCTrb3ZkjSmJt3M0pOZM1Me6HM8vyv6n2Fne9-Ivsg.drTGOvcmJk-socyfhc_yov6mW75K1qEu4i3v7T_KXJkg.JPEG.knicjin/beautiful_nature_wallpaper06.jpg",
+  "https://images.pexels.com/photos/2885320/pexels-photo-2885320.jpeg"
 ];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
